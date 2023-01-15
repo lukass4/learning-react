c1f5fcddebe3bd176c430349fbdc9de7feb28c29
Lukas <luakasjamesr4@gmail.com>
2023-01-15 13:33:01 +0000

    Finished setting up firebase (revisions)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,33 @@
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 export const Navbar = () => {
 	const [user] = useAuthState(auth);
 
+	const signUserOut = async () => {
+		await signOut(auth);
+	};
+
 	return (
-		<div>
-			<Link to="/"> Home </Link>
-			<Link to="/login"> Login </Link>
-			<div>
-				<p>{user?.displayName}</p>
-				<img
-					src={user?.photoURL || ""}
-					width="100"
-					height="100"
-				/>
+		<div className="navbar">
+			<div className="links">
+				<Link to="/"> Home </Link>
+				<Link to="/login"> Login </Link>
+			</div>
+			<div className="user">
+				{user && (
+					<>
+						<img
+							src={user?.photoURL || ""}
+							width="20"
+							height="20"
+						/>
+						<p>{user?.displayName}</p>
+						<button onClick={signUserOut}>Log Out</button>
+					</>
+				)}
 			</div>
 		</div>
 	);
